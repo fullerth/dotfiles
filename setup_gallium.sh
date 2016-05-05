908a86389f26924f97f141f18388611b729cc6cb
Thomas Fuller <thomas@mcfuller.com>
2016-05-05 06:46:30 -0700

    Fix gallium directory removal, add all directories to be removed

--- setup_gallium.sh
+++ setup_gallium.sh
@@ -1,11 +1,19 @@
 set -e
 
 echo "Removing Extra Home Directories"
-extra_dirs="Documents Pictures Templates Videos"
+extra_dirs="Documents Pictures Templates Videos Music Public"
 
+echo "Moving to home directory"
+cd ~
 for dir in $extra_dirs; do
-    if [[ -d "~/${dir}" && ! -L "~/${dir}" ]] ; then
-        rmdir "~/${dir}"
+    echo "Attempting to find if directory exists"
+    if [[ -d "${dir}" ]] ; then
+        echo "Directory Exists"
+        if [[ -L "${dir}" ]] ; then
+        else
+            echo "Removing ${dir}"
+            rmdir "${dir}"
+        fi 
     fi
 done
 
